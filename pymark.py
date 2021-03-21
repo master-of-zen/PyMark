963cd156c2acb35e57585c00d706583f1f4d831c
Zen <true.grenight@gmail.com>
2021-03-22 01:51:54 +0200

    renaming

--- pymark.py
+++ pymark.py
@@ -5,10 +5,12 @@ import math
 import numpy as np
 import argparse
 import re
+import matplotlib
+from matplotlib import plt
 from pathlib import Path
 import subprocess
 from subprocess import PIPE, STDOUT
-from typing import Tuple, List, Dict
+from typing import List, Dict
 from collections import deque
 import json
 
@@ -322,6 +324,71 @@ def print_metrics():
     print("MS-SSIM BD rate:", bdrate(mssim1, mssim0))
 
 
+def plot_range(data, encoder, m_place):
+    dt = [x for x in data if x[2] == cpu]
+    x = sorted([round(x[4]) for x in dt])
+    y = sorted([x[m_place] for x in dt])
+    time = sum([x[1] for x in dt]) / len(dt)
+    f = interpolate.interp1d(x, y, kind="linear")
+    xnew = np.linspace(min(x), max(x), max(x) - min(x))
+    plt.plot(xnew, f(xnew), label=f"{encoder} {cpu}", linewidth=3)
+
+
+def plot(data: Dict, codecs: List):
+
+    for metric, place in [("VMAF", 5), ("PSNR", 6), ("SSIM", 7)]:
+
+        plot_range(aom_data, "Blues", "Aomenc", place)
+        plot_range(vvc_data, "Reds", "VVC", place)
+        # Plot
+
+        plt.xticks(
+            [x for x in range(0, 40000, 100)],
+            [int(x) for x in range(0, 40000, 100)],
+            fontsize=22,
+        )
+
+        if metric in ("VMAF", "PSNR"):
+            plt.yticks([x for x in range(0, 101, 1)], fontsize=28)
+            [plt.axhline(i, color="grey", linewidth=0.5) for i in range(1, 100, 2)]
+            [plt.axhline(i, color="black", linewidth=1) for i in range(0, 100, 2)]
+        else:
+            [
+                plt.axhline(i / 100, color="grey", linewidth=0.5)
+                for i in range(61, 1000, 2)
+            ]
+            [
+                plt.axhline(i / 100, color="black", linewidth=1)
+                for i in range(62, 1000, 2)
+            ]
+            plt.yticks([x / 100 for x in range(0, 1000, 1)], fontsize=28)
+
+        [plt.axvline(i, color="grey", linewidth=0.3) for i in range(0, 40000, 500)]
+        plt.ylabel(metric.capitalize(), size=32)
+        plt.xlabel("Bit rate, Kbps", size=24)
+        plt.title(f"{' vs '.join(codecs)}, latest git 10.12.2020 {metric}", size=28)
+        plt.legend(prop={"size": 19}, loc="lower right")
+
+        # if metric in ('VMAF', 'PSNR'):
+        low_ylim = [x[place] for x in data]
+        low_ylim = np.percentile(sorted(low_ylim), 10)
+
+        bitrates = [x[4] for x in data]
+        plt.xlim(int(np.percentile(bitrates, 10)), int(np.percentile(bitrates, 85)))
+
+        if metric in ("VMAF"):
+            high_ylim = 100
+        elif metric in ("SSIM"):
+            high_ylim = np.percentile(sorted([x[place] for x in data]), 90)
+        elif metric in ("PSNR"):
+            high_ylim = np.percentile(sorted([x[place] for x in data]), 90)
+
+        plt.ylim((low_ylim, high_ylim))
+        plt.margins(0)
+        plt.subplots_adjust(left=0.045, right=0.99, top=0.965, bottom=0.065)
+        plt.show()
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -345,10 +412,21 @@ if __name__ == "__main__":
     if "benchmark" in parsed["function"]:
         enc = parsed["encoder"]
 
+        if not Path(parsed["input"][0]).exists():
+            print("No input file/Can't reach")
+            print(Path(parsed["input"][0]))
+            sys.exit()
+
         if not enc:
             print("No encoder selected")
             sys.exit()
 
         benchmark(parsed["input"][0], enc)
-    elif "plot":
-        pass
+
+    elif "plot" in parsed["function"]:
+        data = Path(parsed["input"])
+
+        if not data.exists():
+            print("No input file/Can't reach")
+            print(Path(parsed["input"][0]))
+            sys.exit()
